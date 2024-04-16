7da083a8f74ce5ea91d3ce4f268e6e974be80110
LukeDiB <141517474+LukeDiB@users.noreply.github.com>
2024-04-16 10:18:01 +0100

    Completed task 4

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,4 @@
-const app = require("../db/app");
+const app = require("../app");
 const data = require("../db/data/test-data/index");
 const db = require("../db/connection");
 const request = require("supertest");
@@ -26,6 +26,8 @@ describe("GET /api/topics", () => {
         });
       });
   });
+});
+describe("Err /api/topics", () => {
   test("404: responds with error message not found!", () => {
     return request(app)
       .get(`/api/topicss!?`)
@@ -44,6 +46,47 @@ describe("GET /api", () => {
       .expect(200)
       .then((res) => {
         expect(res.body).toEqual(endpoints);
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: responds with an object with all relevant properties", () => {
+    return request(app)
+      .get(`/api/articles/1`)
+      .expect(200)
+      .then(({ body }) => {
+        body.forEach((column) => {
+          expect(typeof column.author).toBe("string");
+          expect(typeof column.title).toBe("string");
+          expect(typeof column.article_id).toBe("number");
+          expect(typeof column.body).toBe("string");
+          expect(typeof column.topic).toBe("string");
+          expect(typeof column.created_at).toBe("string");
+          expect(typeof column.votes).toBe("number");
+          expect(typeof column.article_img_url).toBe("string");
+        });
+      });
+  });
+});
+
+describe("Err /api/articles/article_id", () => {
+  test("404: responds with error message not found!", () => {
+    return request(app)
+      .get(`/api/articles/15`)
+      .expect(404)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("id not found!");
+      });
+  });
+  test("400: responds with error message when passed the incorrect type!", () => {
+    return request(app)
+      .get(`/api/articles/not-a-number`)
+      .expect(400)
+      .then(({ body }) => {
+        const { message } = body;
+        expect(message).toBe("invalid id type!");
       });
   });
 });
